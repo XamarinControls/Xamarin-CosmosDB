1d6da468f829e36c4136fe08fa5de223a24fa5f7
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-07-28 20:42:15 +0200

    Updated AppSettings

--- NotesCosmosDB/NotesCosmosDB/NotesCosmosDB/AppSettings.cs
+++ NotesCosmosDB/NotesCosmosDB/NotesCosmosDB/AppSettings.cs
@@ -2,8 +2,8 @@
 {
     public class AppSettings
     {
-        public static readonly string EndpointUri = "https://notescosmosdb.documents.azure.com:443/";
-        public static readonly string PrimaryKey = "O4ppxnOBRMa45y2CPRs469xilgZiuTUYpO3Aoy6Sg9weUcGFAf6N2UsqNjsFRZGKlLGpt3xXBzlQknoGr4cggw==";
+        public static readonly string EndpointUri = "INSERT YOUR ENDPOINT";
+        public static readonly string PrimaryKey = "INSERT YOUR PRIMARY KEY";
         public static readonly string DatabaseName = "NotesDB";
         public static readonly string CollectionName = "Notes";
     }
