4d1fe915cc204153b813d3785f4c322b8a13301b
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-07-28 20:39:38 +0200

    Added Xamarin.Forms CosmoDB Sample

---
+++ NotesCosmosDB/NotesCosmosDB/NotesCosmosDB/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace NotesCosmosDB
+{
+    public class AppSettings
+    {
+        public static readonly string EndpointUri = "https://notescosmosdb.documents.azure.com:443/";
+        public static readonly string PrimaryKey = "O4ppxnOBRMa45y2CPRs469xilgZiuTUYpO3Aoy6Sg9weUcGFAf6N2UsqNjsFRZGKlLGpt3xXBzlQknoGr4cggw==";
+        public static readonly string DatabaseName = "NotesDB";
+        public static readonly string CollectionName = "Notes";
+    }
+}
